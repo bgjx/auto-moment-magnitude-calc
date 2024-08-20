9b5119e3fd4812aa28fe18e00c121fcf6bf7537b
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2024-08-20 23:57:39 +0700

    change the optimizer code name

--- fit_spectral.py
+++ fit_spectral.py
@@ -1,21 +1,41 @@
+from typing import Dict, Tuple, List, Optional
 import numpy as np
 from skopt import gp_minimize
 from skopt.space import Real , Integer
 from skopt.utils import use_named_args
 from scipy.stats import uniform
 
-# function for spectrum windowing within spesific f_band
-def window_band(frequencies, spectrums, f_min, f_max):
+
+def window_band(frequencies: np.ndarray, spectrums: np.ndarray, f_min: float, f_max: float) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Extracts a subset of the frequency spectrum within a specified frequency band.
+
+    Args:
+        frequencies (np.ndarray): Array of frequency values.
+        spectrums (np.ndarray): Array of spectral values corresponding to the frequencies.
+        f_min (float): Minimum frequency of the band to extract (inclusive).
+        f_max (float): Maximum frequency of the band to extract (inclusive).
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: 
+            - freq: Array of frequencies within the specified band.
+            - spec: Array of spectral values corresponding to the extracted frequencies.
+    """
     indices = np.where((frequencies >= f_min) & (frequencies <= f_max))
     freq = frequencies[indices]
     spec = spectrums[indices]
     return  freq, spec
     
-# function for calculating the source spectrum (spectrum model) 
-def calculate_source_spectrum(frequencies, omega_0, Q, corner_frequency, 
-    traveltime):
+    
+    
+def calculate_source_spectrum(frequencies: np.ndarray,
+                                omega_0: float,
+                                Q: float,
+                                corner_frequency: float,
+                                traveltime: float
+                                ) -> np.ndarray:
     """
-    After Abercrombie (1995) and Boatwright (1980).
+    Calculate the source spectrum based on Abercrombie (1995) and Boatwright (1980).
     Abercrombie, R. E. (1995). Earthquake locations using single-station deep
     borehole recordings: Implications for microseismicity on the San Andreas
     fault in southern California. Journal of Geophysical Research, 100,
@@ -23,21 +43,50 @@ def calculate_source_spectrum(frequencies, omega_0, Q, corner_frequency,
     Boatwright, J. (1980). A spectral theory for circular seismic sources,
     simple estimates of source dimension, dynamic stress drop, and radiated
     energy. Bulletin of the Seismological Society of America, 70(1).
-    The used formula is:
-        Omega(f) = (Omege(0) * e^(-pi * f * T / Q)) / (1 + (f/f_c)^4) ^ 0.5
-    :param frequencies: Input array to perform the calculation on.
-    :param omega_0: Low frequency amplitude in [meter x second].
-    :param corner_frequency: Corner frequency in [Hz].
-    :param Q: Quality factor.
-    :param traveltime: Traveltime in [s].
+    
+    Args:
+        frequencies (np.ndarray): Array of frequency values.
+        omega_0 (float): Value of the omega zero (spectral flat level).
+        Q (float): Quality factor for amplitude attenuation.
+        corner_frequency (float): The corner frequency.
+        traveltime(float): Value of the phase travel time.
+        
+    Returns:
+        np.ndarray: An array of theoretical calculated source spectrum.
     """
     num = omega_0 * np.exp(-np.pi * frequencies * traveltime / Q)
     denom = (1 + (frequencies / corner_frequency) ** 4)**0.5
     return num / denom
-
-
-# fitting spectrum using Grid search algorithm
-def fit_spectrum_systematic (frequencies, spectrums, traveltime, f_min, f_max):
+    
+    
+    
+def fit_spectrum_systematic (frequencies: np.ndarray,
+                                spectrums: np.ndarray,
+                                traveltime: float,
+                                f_min: float,
+                                f_max: float
+                                ) -> Tuple[float, float, float, float, np.ndarray, np.ndarray]:
+    """
+    A systematic parameter optimization process using grid search to obtain the best-fitting model for the observed spectrum.
+    
+    Args:
+        frequencies (np.ndarray): Array of frequency values.
+        omega_0 (float): Value of the omega zero (spectral flat level).
+        Q (float): Quality factor for amplitude attenuation.
+        corner_frequency (float): The corner frequency.
+        traveltime(float): Value of the phase travel time.
+        f_min (float): Minimum frequency of the band to be calculated.
+        f_max (float): Maximum frequency of the band to be calculated.
+        
+    Returns:
+        Tuple[float, float, float, float, np.ndarray, np.ndarray]:
+            - omega_0_fit: The optimized value of omega 0 (spectral flat level) obtained from fitting the model.
+            - q_factor_fit: The optimized value of quality factor obtained from fitting the model.
+            - f_c_fit: The optimized value of frequency corner obtained from fitting the model.
+            - best_rms_e : The best rms value obtained from model fitting process.
+            - x_tuned : Array of frequencies in spesific resolution and frequencies band.
+            - y_tuned: Array of spectrum computed from the model equation using optimized variable.
+    """
     # windowing frequencies and spectrum within f band    
     freq, spectrum = window_band(frequencies, spectrums, f_min, f_max)
     
@@ -48,7 +97,7 @@ def fit_spectrum_systematic (frequencies, spectrums, traveltime, f_min, f_max):
     f_c = np.linspace(0.75, 30, 50)
     
     # rms and error handler
-    error = np.inf
+    best_rms_e = np.inf
     
     # define callable function
     def f(freqs, omega, qfactor, f_cor):
@@ -60,8 +109,8 @@ def fit_spectrum_systematic (frequencies, spectrums, traveltime, f_min, f_max):
             for k in range(len(f_c)):
                 fwd = f(freq, omega_0[i], Q_factor[j], f_c[k])
                 rms_e = np.sqrt(np.mean((fwd - spectrum)**2))
-                if rms_e < error:
-                    error = rms_e
+                if rms_e < best_rms_e:
+                    best_rms_e = rms_e
                     omega_0_fit = omega_0[i]
                     Q_factor_fit = Q_factor[j]
                     f_c_fit = f_c[k]
@@ -70,10 +119,37 @@ def fit_spectrum_systematic (frequencies, spectrums, traveltime, f_min, f_max):
     x_tuned = np.linspace(0.75, 100, 100)
     y_tuned = f(x_tuned, omega_0_fit, Q_factor_fit, f_c_fit) 
                     
-    return omega_0_fit, Q_factor_fit, f_c_fit, rms_e, x_tuned, y_tuned
-
-# fitting spectrum using Random search algorithm
-def fit_spectrum_stochastic (frequencies, spectrums, traveltime, f_min, f_max):
+    return omega_0_fit, Q_factor_fit, f_c_fit, best_rms_e, x_tuned, y_tuned
+    
+    
+    
+def fit_spectrum_stochastic (frequencies: np.ndarray,
+                                spectrums: np.ndarray,
+                                traveltime: float,
+                                f_min: float,
+                                f_max: float
+                                ) -> Tuple[float, float, float, float, np.ndarray, np.ndarray]:
+    """
+    A stochastic parameter optimization process using random search to obtain the best-fitting model for the observed spectrum.
+    
+    Args:
+        frequencies (np.ndarray): Array of frequency values.
+        omega_0 (float): Value of the omega zero (spectral flat level).
+        Q (float): Quality factor for amplitude attenuation.
+        corner_frequency (float): The corner frequency.
+        traveltime(float): Value of the phase travel time.
+        f_min (float): Minimum frequency of the band to be calculated.
+        f_max (float): Maximum frequency of the band to be calculated.
+        
+    Returns:
+        Tuple[float, float, float, float, np.ndarray, np.ndarray]:
+            - omega_0_fit: The optimized value of omega 0 (spectral flat level) obtained from fitting the model.
+            - q_factor_fit: The optimized value of quality factor obtained from fitting the model.
+            - f_c_fit: The optimized value of frequency corner obtained from fitting the model.
+            - best_rms_e : The best rms value obtained from model fitting process.
+            - x_tuned : Array of frequencies in spesific resolution and frequencies band.
+            - y_tuned: Array of spectrum computed from the model equation using optimized variable.
+    """
     # windowing frequencies and spectrum within f band    
     freq, spectrum = window_band(frequencies, spectrums, f_min, f_max)
     
@@ -84,7 +160,7 @@ def fit_spectrum_stochastic (frequencies, spectrums, traveltime, f_min, f_max):
     f_c_range = (0.75, 30)
     
     # rms and error handler
-    best_params = {'omega_0': None, 'Q_factor': None, 'f_c': None}
+    omega_0_fit, Q_factor_fit, f_c_fit = np.inf, np.inf, np.inf
     best_rms_e = np.inf
     
     # define callable function
@@ -104,18 +180,18 @@ def fit_spectrum_stochastic (frequencies, spectrums, traveltime, f_min, f_max):
         
         if rms_e < best_rms_e:
             best_rms_e = rms_e
-            best_params['omega_0'] = omega_0
-            best_params['Q_factor'] = Q_factor
-            best_params['f_c'] = f_c
+            omega_0_fit = omega_0
+            Q_factor_fit = Q_factor
+            f_c_fit = f_c
                     
     # calculate the fitted power spectral density from tuned parameter
     x_tuned = np.linspace(0.75, 100, 100)
-    y_tuned = f(x_tuned, best_params['omega_0'], best_params['Q_factor'], best_params['f_c']) 
+    y_tuned = f(x_tuned, omega_0_fit, Q_factor_fit, f_c_fit) 
                     
-    return best_params['omega_0'], best_params['Q_factor'], best_params['f_c'], best_rms_e, x_tuned, y_tuned
-
-
-# fitting spectrum with bayesian optimization
+    return omega_0_fit, Q_factor_fit, f_c_fit, best_rms_e, x_tuned, y_tuned
+    
+    
+    
 def fit_spectrum_bayes_opt (frequencies, spectrums, traveltime, f_min, f_max):
     # windowing frequencies and spectrum within f band    
     freq, spectrum = window_band(frequencies, spectrums, f_min, f_max)
